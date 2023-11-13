0ff6582bd9ba90ae3126f69b6549b00736e5d472
Carlston Rebelo <130921730+Carlston325@users.noreply.github.com>
2023-11-13 21:00:51 +0000

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// BUTTON CLICK
+// Add Media --BUTTON CLICK--
 $("#add-media-Div").click (function () {
 
-    let activeButton = $("#add-media");
+    let activeButton = $("#add-media-Div");
     let textFlash = $(".btn");
     
     activeButton.addClass("btn-shadow");
@@ -13,10 +13,78 @@ $("#add-media-Div").click (function () {
     }, 200);
 });
 
-// Change Section (Photo - Video)
-document.getElementById("select-section").addEventListener("click", function() {
-    let sectionSelectButton = document.getElementById("select-section");
+let arrowButton = $("#select-section-Div");
+let image = $("#select-section")
+let movingButton = true;
 
-    sectionSelectButton.classList.add("move-left");
-    sectionSelectButton.classList.remove("move-right");
+let photoSection = $("#photo-Div");
+let videoSection = $("#video-Div");
+
+// Change --ENTIRE-- Section (Photo - Video)
+let webSize = window.matchMedia("(max-width: 650px)");
+
+if (webSize.matches) {
+    $("#select-section-Div").remove();
+    
+    $("#body").after("<div class='section-change-btn'><h3> <span class='green'>PHOTOS</span> <span style='color: white;'>/</span> <span class='blue'>VIDEOS</span> </h3>");
+}
+
+$(".section-change-btn").click (function () {
+
+    let sectionChangeBTN = $(".section-change-btn");
+    sectionChangeBTN.addClass("btn-shadow");
+    
+    setTimeout(function() {
+        sectionChangeBTN.removeClass("btn-shadow");
+    }, 100);
+
+    if (movingButton) {
+
+        videoSection.css("z-index", "1");
+        photoSection.css("z-index", "0");
+
+        $(".blue").addClass("text-flash");
+
+        setTimeout(function() {
+            $(".blue").removeClass("text-flash");
+        }, 100);
+    } else {
+
+        videoSection.css("z-index", "0");
+        photoSection.css("z-index", "1");
+
+        $(".green").addClass("text-flash");
+
+        setTimeout(function() {
+            $(".green").removeClass("text-flash");
+        }, 100);
+    }
+
+    movingButton = !movingButton;   
+});
+
+// Change Section --BUTTON-- (Photo - Video)
+arrowButton.click(function() {
+
+    if (movingButton) {
+        
+        image.attr("src", "./assets/svg/caret-" + "right" + "-square-fill.svg");
+
+        videoSection.css("z-index", "1");
+        photoSection.css("z-index", "0");
+
+        arrowButton.css("text-align", "start");
+        image.css("background-color", "rgb(129, 255, 112)");
+    } else {
+
+        image.attr("src", "./assets/svg/caret-" + "left" + "-square-fill.svg");
+        
+        videoSection.css("z-index", "0");
+        photoSection.css("z-index", "1");
+
+        arrowButton.css("text-align", "end");
+        image.css("background-color", "rgb(112, 234, 255)");
+    }
+
+    movingButton = !movingButton;
 });
